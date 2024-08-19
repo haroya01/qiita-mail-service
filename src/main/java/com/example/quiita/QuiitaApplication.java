903b881d8646f63058501e5121c663c9edd68ac6
gimdonghyeon <haroya01@naver.com>
2024-08-20 02:40:37 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/example/quiita/QuiitaApplication.java
@@ -0,0 +1,13 @@
+package com.example.quiita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuiitaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quiita/QuiitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quiita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuiitaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
